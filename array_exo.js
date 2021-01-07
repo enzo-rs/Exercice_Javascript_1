aa58d3dca399c70a5788a5b4035ed35b3186511b
enzo-rs <enzorojoall@gmail.com>
2021-01-07 10:27:14 +0100

    Quatrième exo

---
+++ array_exo.js
@@ -0,0 +1,10 @@
+
+function array(arr) {
+    let str = "";
+    for (let i=0; i<arr.length; i++) {
+        str += arr[i] + "\n"
+    }
+    console.log(str + "Nombre: " + arr.length)
+}
+
+array([1, 2, 2, 3, 4]);

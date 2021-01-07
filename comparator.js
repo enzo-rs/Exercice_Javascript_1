6b713dca3b5854be3ae3625b18ce9a3c611006d7
enzo-rs <enzorojoall@gmail.com>
2021-01-07 10:31:32 +0100

    Cinquieme exercice

---
+++ comparator.js
@@ -0,0 +1,13 @@
+
+function comparator(arg1, arg2) {
+
+    if (arg1 === arg2) {
+        console.log("Same value and same type.")
+    }
+    else if (arg1 == arg2) {
+        console.log("Same value")
+    }
+
+}
+
+comparator(5, "5")
